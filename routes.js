1603df42367c849257f7d20b8395ad9d27126055
Smriti Kaushal <smritikaushal24@gmail.com>
2024-06-19 00:03:11 +0530

    improved dev workflow

--- routes.js
+++ routes.js
@@ -1,52 +1,50 @@
-// routes.js
-const express = require('express');
-const router = express.Router();
+const fs = require('fs');
 
-// Dummy user data
-const users = [
-    { id: 1, name: 'smriti1' },
-    { id: 2, name: 'smriti2' },
-    { id: 3, name: 'smriti3' }
-];
-
-// Route for "/"
-router.get('/', (req, res) => {
-    res.send(`
-        <html>
-            <body>
-                <h1>Welcome to our website!</h1>
-                <form action="/create-user" method="POST">
-                    <input type="text" name="username" placeholder="Enter username" required />
-                    <button type="submit">Create User</button>
-                </form>
-            </body>
-        </html>
-    `);
-});
-
-// Route for "/users"
-router.get('/users', (req, res) => {
-    let userList = '<ul>';
-    users.forEach(user => {
-        userList += `<li>${user.name}</li>`;
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
     });
-    userList += '</ul>';
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+};
 
-    res.send(`
-        <html>
-            <body>
-                <h1>User List</h1>
-                ${userList}
-            </body>
-        </html>
-    `);
-});
+// module.exports = requestHandler;
 
-// Route for handling form submission
-router.post('/create-user', (req, res) => {
-    const username = req.body.username;
-    console.log('New user:', username);
-    res.redirect('/');
-});
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
 
-module.exports = router;
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
+
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
